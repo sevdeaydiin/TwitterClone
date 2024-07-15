eda19bf3e4874a0ef23097ba6df35e1b74c18e6c
Sevde Aydın <sevdenuraydn7@gmail.com>
2024-07-15 16:13:12 +0300

    Initial Commit

---
+++ twitter-clone-app/twitter_clone_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitter_clone_appApp.swift
+//  twitter-clone-app
+//
+//  Created by Sevde Aydın on 15.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct twitter_clone_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
