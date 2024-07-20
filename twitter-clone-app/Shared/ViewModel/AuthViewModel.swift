47d5e8ee3fedf69c4fa8d0ba7883911ed6b069e6
Sevde Aydın <sevdenuraydn7@gmail.com>
2024-07-21 01:35:00 +0300

    Added AuthViewModel and connected database

--- twitter-clone-app/Shared/ViewModel/AuthViewModel.swift
+++ twitter-clone-app/Shared/ViewModel/AuthViewModel.swift
@@ -5,4 +5,50 @@
 //  Created by Sevde Aydın on 21.07.2024.
 //
 
-import Foundation
+import SwiftUI
+
+class AuthViewModel: ObservableObject {
+     
+    func login() {
+        
+    }
+    
+    func register(reqBody: [String: Any]) {
+        
+        let session = URLSession.shared
+        
+        var request = URLRequest(url: URL(string: "http://localhost:3000/users")!)
+        
+        request.httpMethod = "POST"
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: reqBody, options: .prettyPrinted)
+        } catch let error {
+            print(error)
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        let task = session.dataTask(with: request) { data, res, err in
+            guard err == nil else { return }
+            guard let data = data else { return }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    
+                }
+            } catch let error {
+                print(error)
+            }
+        }
+            
+        task.resume()
+        
+    }
+    
+    func logout() {
+        
+    }
+    
+}
