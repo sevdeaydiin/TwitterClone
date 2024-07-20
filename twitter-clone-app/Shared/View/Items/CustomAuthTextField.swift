03c84ccc412fe75a31cd256d8bd985e9b1796a3a
Sevde Aydın <sevdenuraydn7@gmail.com>
2024-07-21 01:34:11 +0300

    Updated

--- twitter-clone-app/Shared/View/Items/CustomAuthTextField.swift
+++ twitter-clone-app/Shared/View/Items/CustomAuthTextField.swift
@@ -8,11 +8,27 @@
 import SwiftUI
 
 struct CustomAuthTextField: View {
+    
+    var placeholder: String
+    @Binding var text: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ZStack(alignment: .leading, content: {
+                if text.isEmpty {
+                    Text(placeholder)
+                        .foregroundStyle(.gray)
+                }
+                TextField("", text: $text)
+                    .frame(height: 45)
+                    .foregroundStyle(Color(red: 29 / 255, green: 161 / 255, blue: 242 / 255))
+            })
+            
+            Rectangle()
+                .frame(height: 1, alignment: .center)
+                .foregroundStyle(.gray)
+                .padding(.top, -2)
+        }
+        .padding(.horizontal)
     }
-}
-
-#Preview {
-    CustomAuthTextField()
 }
