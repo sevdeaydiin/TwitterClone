566ae8e4718c27ba2fcfce09c97ff9fa7668f1b1
Sevde Aydın <sevdenuraydn7@gmail.com>
2024-07-25 16:06:58 +0300

    Added logout func

--- twitter-clone-app/Shared/ViewModel/AuthViewModel.swift
+++ twitter-clone-app/Shared/ViewModel/AuthViewModel.swift
@@ -11,6 +11,25 @@ class AuthViewModel: ObservableObject {
     
     @Published var isAuthenticated: Bool = false
     @Published var currentUser: User?
+    
+    init() {
+        let defaults = UserDefaults.standard
+        let token = defaults.object(forKey: "jsonwebtoken")
+        //defaults.removeObject(forKey: "jsonweboken")
+        
+        if token != nil {
+            isAuthenticated = true
+             
+            if let userId = defaults.object(forKey: "userid") {
+                fetchUser(userId: userId as! String)
+                print("User fetched")
+            }
+        } else {
+            isAuthenticated = false
+        }
+    }
+    
+    static let shared = AuthViewModel()
      
     func login(email: String, password: String) {
         
@@ -47,8 +66,36 @@ class AuthViewModel: ObservableObject {
         }
     }
     
+    func fetchUser(userId: String) {
+        AuthServices.fetchUser(id: userId) { result in
+            switch result {
+            case .success(let data):
+                guard let user = try? JSONDecoder().decode(User.self, from: data) else { return }
+                
+                DispatchQueue.main.async {
+                    UserDefaults.standard.setValue(user.id, forKey: "userid")
+                    self.isAuthenticated = true
+                    self.currentUser = user
+                    print(user)
+                }
+                
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
     func logout() {
+        let defaults = UserDefaults.standard
+        let dictionary = defaults.dictionaryRepresentation()
         
+        dictionary.keys.forEach { key in
+            defaults.removeObject(forKey: key)
+        }
+        
+        DispatchQueue.main.async {
+            self.isAuthenticated = false
+        }
     }
     
 }
