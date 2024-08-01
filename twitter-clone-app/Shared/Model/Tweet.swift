40a50c7b3c5318d9726373776f69334d83d941be
Sevde Aydın <sevdenuraydn7@gmail.com>
2024-08-01 23:35:23 +0300

    Added tweet model

--- twitter-clone-app/Shared/Model/Tweet.swift
+++ twitter-clone-app/Shared/Model/Tweet.swift
@@ -5,4 +5,16 @@
 //  Created by Sevde Aydın on 1.08.2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct Tweet: Identifiable, Decodable {
+
+    let _id: String
+    var id: String {
+        return _id
+    }
+    let text: String
+    let userId: String
+    let username: String
+    let user: String
+}
