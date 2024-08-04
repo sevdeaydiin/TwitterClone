4e0fc7290b6ecc18d0b3f7688085f1b310276a2c
Sevde Aydın <sevdenuraydn7@gmail.com>
2024-08-04 22:09:47 +0300

    Image Picker added

---
+++ twitter-clone-app/Shared/Extensions/ImagePicker.swift
@@ -0,0 +1,43 @@
+//
+//  ImagePicker.swift
+//  twitter-clone-app
+//
+//  Created by Sevde Aydın on 4.08.2024.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) var mode
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            guard let image = info[.originalImage] as? UIImage else { return }
+            
+            self.parent.image = image
+            self.parent.mode.wrappedValue.dismiss()
+        }
+    }
+}
--- twitter-clone-app/Shared/View/Post/CreateTweetView.swift
+++ twitter-clone-app/Shared/View/Post/CreateTweetView.swift
@@ -12,6 +12,9 @@ struct CreateTweetView: View {
     @Binding var show: Bool
     @State var text = ""
     @ObservedObject var viewModel = CreateTweetViewModel()
+    @State var imagePickerPresented: Bool = false
+    @State var selectedImage: UIImage?
+    @State var postImage: Image?
     
     var body: some View {
         VStack {
@@ -40,3 +43,48 @@ struct CreateTweetView: View {
             
             MultilineTextField(text: $text)
             //TextField("What's happening?", text: $text)
+
+            if postImage == nil {
+                Button {
+                    self.imagePickerPresented.toggle()
+                } label: {
+                    Image(systemName: "plus.circle")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 70, height: 70)
+                        .clipped()
+                        .padding(.top)
+                        .foregroundStyle(.black)
+                }.sheet(isPresented: $imagePickerPresented) {
+                    loadImage()
+                } content: {
+                    ImagePicker(image: $selectedImage)
+                }
+            }
+            
+            else if let image = postImage {
+                VStack {
+                    HStack(alignment: .top) {
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .padding(.horizontal)
+                            .frame(width: Sizes.width * 0.9)
+                            .cornerRadius(16)
+                            .clipped()
+                        Spacer()
+                    }.padding(.bottom, 70)
+                    Spacer()
+                }
+            }
+            
+        }.padding()
+    }
+}
+
+extension CreateTweetView {
+    func loadImage() {
+        guard let selectedImage = selectedImage else { return }
+        postImage = Image(uiImage: selectedImage)
+    }
+}
